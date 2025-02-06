5355bdc8bbc4a0fb3b2392e22c5c3f3eb65f25c4
Richard Nixon <rnixonaf@gmail.com>
2025-02-06 19:40:40 +0000

    Done 1st day -Richard/Kainat

--- src/question.js
+++ src/question.js
@@ -1,4 +1,20 @@
 class Question {
+
+    constructor(text, choices, answer, difficulty) {
+        
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+        this.difficulty = difficulty;
+    }
+
+    shuffleChoices(){
+        for(let i = this.choices.length -1; i >= 1; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.choices[i], this.choices[j]] = [this.choices[j], this.choices[i]];
+        }
+
+    }
     // YOUR CODE HERE:
     //
     // 1. constructor (text, choices, answer, difficulty)
--- src/quiz.js
+++ src/quiz.js
@@ -1,4 +1,49 @@
 class Quiz {
+    constructor(questions, timeLimit, timeRemaining){
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining =  timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
+
+    getQuestion (){
+        return this.questions[this.currentQuestionIndex];
+    };
+
+    moveToNextQuestion(){
+        this.currentQuestionIndex++; 
+    }
+
+    shuffleQuestions(){
+        for(let i = this.questions.length -1; i >= 1; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
+        }
+    }
+
+    checkAnswer(answer){
+        this.correctAnswers++; 
+    }
+
+    hasEnded(){
+        if(this.currentQuestionIndex < this.questions.length){
+            return false;
+        } else {
+            return true;
+        }
+    } 
+
+
+    filterQuestionsByDifficulty(difficulty){
+     
+    }
+
+    averageDifficulty(){
+   
+    }
+
+
     // YOUR CODE HERE:
     //
     // 1. constructor (questions, timeLimit, timeRemaining)
